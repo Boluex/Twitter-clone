e6b01f43cda418f0d4ed873e802d22de18f324e2
Boluex <awomewekolawole1.com>
2025-06-05 23:14:05 +0100

    updated

--- social/urls.py
+++ social/urls.py
@@ -20,5 +20,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('twitter.urls')),
-    path('',include('users.urls'))
+    path('',include('users.urls')),
+    path('', include('django_prometheus.urls')),
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
